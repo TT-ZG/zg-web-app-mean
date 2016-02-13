f17b5ce4ea098a10cedd0fd5f2d39cc05d8d4804
jeffrey mclemore <jmclemore368@gmail.com>
2016-02-13 15:53:30 -0500

    more uirouter

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -7,25 +7,60 @@ angular.module('app.routes', ['ui.router'])
 
     $stateProvider
 
+      // HOME STATES AND NESTED VIEWS ========================================
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/views/templates/user/home.html'
+      });
+
+
+
+
+
+
+
+      /*
         // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-            templateUrl: 'app/views/partial-home.html'
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'app/views/templates/temp/partial-home.html'
         })
 
         // nested list with custom controller
-        .state('home.list', {
+        .state('dashboard.list', {
         url: '/list',
-        templateUrl: 'app/views/partial-home-list.html',
+        templateUrl: 'app/views/templates/temp/partial-home-list.html',
         controller: function($scope) {
             $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
         }
         })
 
-    // nested list with just some random string data
-    .state('home.paragraph', {
-        url: '/paragraph',
-        template: 'I could sure use a drink right now.'
-    })
+        // nested list with just some random string data
+        .state('dashboard.paragraph', {
+          url: '/paragraph',
+          template: 'I could sure use a drink right now.'
+        })
+
+
+        .state('about', {
+        url: '/about',
+        views: {
+
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'app/views/templates/temp/partial-about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'This column has its own view!' },
+
+            // for column two, we'll define a separate controller
+            'columnTwo@about': {
+              template: 'This column also has its own view'
+              /*
+                templateUrl: 'table-data.html',
+                controller: 'scotchController'
+            }
+        }
+
+      });*/
 
 });

7e1c4535e0ea88252f20d273dda9454da2acb439
jeffrey mclemore <jmclemore368@gmail.com>
2016-03-05 00:20:52 -0500

    deleted extraneous code

--- public/js/main.js
+++ public/js/main.js
@@ -8,21 +8,4 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 // click anywhere outside to close the menu
 $(document).on('click',function(){
 	$('.collapse').collapse('hide');
-})
-
-function readURL(input) {
-  console.log('init');
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#blah').attr('src', e.target.result);
-        }
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$("#imgInp").change(function(){
-    readURL(this);
 });

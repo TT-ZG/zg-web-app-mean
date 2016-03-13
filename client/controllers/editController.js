d19a1fe7c8e1fe378a93d747f09e1ec8c396f41b
jeffrey mclemore <jmclemore368@gmail.com>
2016-03-12 20:07:51 -0500

    finished editCtrl modularization

--- client/controllers/editController.js
+++ client/controllers/editController.js
@@ -4,7 +4,7 @@
   // this controller handles the editing of brothers
   var editController = function($state, $stateParams, crudFactory, $scope, fileUpload){
 
-   // =============Here are setting the hardcoded form options==================
+   // ====================Set the hardcoded form options========================
     var brother = this;
 
     // The options available for standings
@@ -27,8 +27,17 @@
       { property : "gpa", value: "On Request" },
     ];
 
+    // ===========These functions increase the user experience=================
+    // start the spinner on the page
+    brother.startSpinner = function(){
+      brother.processing = true;
+    };
+    // end the spinner on the page
+    brother.endSpinner = function(){
+      brother.processing = false;
+    };
 
-    // =========These functions get the brothers info from the server===========
+    // =========These functions GET the brothers info from the server===========
     // get a brothers info
     brother.get = function(id){
 
@@ -50,129 +59,93 @@
         .then(function(res){
           if (res.data.success){
             brother.setPicture(res);
-            brother.setPictureMessages(res);
           }
           brother.setPictureMessages(res);
           brother.endSpinner();
         });
      };
 
-     // ==========These functions set the brothers info from the server==========
-     // set the discrete json data
-     brother.setInfo = function(res){
-       brother.userData = res.data.info;
-       brother.userData.graduation = new Date(brother.userData.graduation);
-       brother.setInternships();
-     }
-     // set the base 64 encoded image
-     brother.setPicture = function(res){
-       $scope.image_source = "data:image/jpeg;base64, " + res.data.data;
-     }
-     // set the dynamic rows for the internships
-     brother.setInternships = function(){
-       // if we shaved off the example because they entered nothing, reset
-       if (brother.userData.internships.length === 0){
-         brother.generateInternships();
-       }
-     };
-
-     // =======These functions set the response messages from the server=========
-     // set the server response messages for the discrete json messages
-     brother.setDataMessages = function(res){
-       if(res.data.success){
-         console.log('Success:' + res.data.message);
-         brother.dataMessage = res.data.message;
-         brother.dataError = '';
-       }
-       else{
-         console.log('Error:' + res.data.message);
-         brother.dataError = res.data.message;
-         brother.dataMessage = '';
-       }
-     };
-     // set the server response messages for the picture
-     brother.setPictureMessages = function(res){
-       if(res.data.success){
-         console.log('Success:' + res.data.message);
-         brother.pictureMessage = res.data.message;
-         brother.pictureError = '';
-       }
-       else{
-         console.log('Error:' + res.data.message);
-         brother.pictureError = res.data.message;
-         brother.pictureMessage = '';
-       }
-     };
-     // reset all messages on the form
-     brother.resetMessages = function(){
-       brother.dataMessage = '';
-       brother.pictureMessage = '';
-       brother.dataError = '';
-       brother.pictureError = '';
-     };
-
-     // ===========These functions increase the user experience=================
-     // start the spinner on the page
-     brother.startSpinner = function(){
-       brother.processing = true;
-     };
-     // end the spinner on the page
-     brother.endSpinner = function(){
-       brother.processing = false;
-     };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // =========================================================================
-    // =========================================================================
-    // edit a specific user
+    // ===========These functions PUT a brothers info to the server=============
+    // save a brothers info
     brother.saveBrother = function() {
-      brother.dataMessage = '';
-      brother.pictureMessage = '';
-      brother.processing = true;
-
-      //chop off the last item from the array if it is blank
-      var lastItem = brother.userData.internships.length-1;
-      if (brother.userData.internships[lastItem].name === undefined){
-        brother.userData.internships.splice(lastItem);
-      }
+      brother.resetMessages();
+      brother.startSpinner();
+      brother.removeLastFromArray();
 
       // update the brothers info
       crudFactory.update($stateParams.brotherid, brother.userData)
-      .success(function(res){
-        if (res.success){
-          brother.dataMessage = res.message;
-          brother.updatePicture($stateParams.brotherid);
-          // if we chopped off the last internship
-          if (brother.userData.internships.length === 0)
-            brother.generateInternships();
-        }
-        console.log(res.message);
-      })
-      .error(function(res){
-        console.log(res.message);
-      })
+        .then(function(res){
+          if (res.data.success){
+            brother.savePicture($stateParams.brotherid);
+          }
+          brother.setDataMessages(res);
+          brother.setInternships();
+        });
+    };
+    // save a brothers picture
+    brother.savePicture = function(brotherId){
+      var file = $scope.myFile;
+      var method = 'PUT';
+      var uploadUrl = "/api/pictures/" + brotherId;
+
+      // save the brothers picture using special service
+      fileUpload.upload(method, file, uploadUrl, function(data, status, headers, config){
+        // the response object is a little different here
+        var res = {};
+        res.data = data;
+        brother.setPictureMessages(res);
+        brother.processing = false;
+      });
     };
 
-    // =========================================================================
-    // =========================================================================
+    // ==========These functions set the brothers info from the server=========
+    // set the discrete json data
+    brother.setInfo = function(res){
+      brother.userData = res.data.info;
+      brother.userData.graduation = new Date(brother.userData.graduation);
+      brother.setInternships();
+    }
+    // set the base 64 encoded image
+    brother.setPicture = function(res){
+      $scope.image_source = "data:image/jpeg;base64, " + res.data.data;
+    }
+    // set the dynamic rows for the internships
+    brother.setInternships = function(){
+      // if we shaved off the example because they entered nothing, reset
+      if (brother.userData.internships.length === 0){
+        brother.resetInternships();
+      }
+    };
+
+    // =======These functions set the response messages from the server=========
+    // set the server response messages for the discrete json messages
+    brother.setDataMessages = function(res){
+      if(res.data.success){
+        console.log('Success:' + res.data.message);
+        brother.dataMessage = res.data.message;
+        brother.dataError = '';
+      }
+      else{
+        console.log('Error:' + res.data.message);
+        brother.dataError = res.data.message;
+        brother.dataMessage = '';
+      }
+    };
+    // set the server response messages for the picture
+    brother.setPictureMessages = function(res){
+      if(res.data.success){
+        console.log('Success:' + res.data.message);
+        brother.pictureMessage = res.data.message;
+        brother.pictureError = '';
+      }
+      else{
+        console.log('Error:' + res.data.message);
+        brother.pictureError = res.data.message;
+        brother.pictureMessage = '';
+      }
+    };
+
+    // ==========These functions help angular handle file objects===============
     // for image preview
     $scope.setFile = function(element) {
       $scope.currentFile = element.files[0];
@@ -186,54 +159,52 @@
       reader.readAsDataURL(element.files[0]);
     }
 
-    // =========================================================================
-    // =========================================================================
-    brother.updatePicture = function(brotherId){
-      var file = $scope.myFile;
-      // set the format in which we update the picture
-      var method = 'PUT';
-      var uploadUrl = "/api/pictures/" + brotherId;
 
-      // save the brothers picture using special service
-      fileUpload.upload(method, file, uploadUrl, function(data, status, headers, config){
-        console.log(data.message);
-        brother.pictureMessage = data.message;
-        brother.processing = false;
-      });
-    };
-
-    // =========================================================================
-    // =========================================================================
+    // ==========These functions reset form data as necessary===================
     // default internships (none)
-    brother.generateInternships = function(){
+    brother.resetInternships = function(){
       brother.userData.internships = [{id: '1'}];
     }
+    // reset all messages on the form
+    brother.resetMessages = function(){
+      brother.dataMessage = '';
+      brother.pictureMessage = '';
+      brother.dataError = '';
+      brother.pictureError = '';
+    };
 
-    // only add positions if the name property of the last position is not null
+    // ==========These functions help handle dynamic row generation=============
+    // only add if last position is not null
     brother.addNewChoice = function() {
       var newItemNo = brother.userData.internships.length+1;
       if (brother.userData.internships[brother.userData.internships.length-1].name != null){
         brother.userData.internships.push({'id':newItemNo});
       }
     };
-
     // if the first item is the last item, simply do the default
     brother.removeChoice = function() {
       var lastItem = brother.userData.internships.length-1;
       if (lastItem === 0)
-        brother.generateInternships();
+        brother.resetInternships();
       else
         brother.userData.internships.splice(lastItem);
     };
+    // remove the last item from the array if it is undefined
+    brother.removeLastFromArray = function(){
+      //chop off the last item from the array if it is blank
+      var lastItem = brother.userData.internships.length-1;
+      if (brother.userData.internships[lastItem].name === undefined){
+        brother.userData.internships.splice(lastItem);
+      }
+    }
 
-    // =========================================================================
-    // =========================================================================
+    // ==========Get the info for the brother given the url parameter===========
     // get the data when the state is loaded
     brother.get($stateParams.brotherid);
   };
 
-  // =========================================================================
-  // =========================================================================
+
+  // ==========================End of controller================================
   // For minification purposes
   editController.$inject = ['$state', '$stateParams', 'crudFactory', '$scope', 'fileUpload'];
 

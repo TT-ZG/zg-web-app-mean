80f2e48a2e424e9a9157f1ffdf50a2b432fe635d
jeffrey mclemore <jmclemore368@gmail.com>
2016-03-14 17:46:45 -0400

    breaking changes

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -73,37 +73,6 @@
       main.current = '';
       main.loggedIn = false;
     };
-
-    // =========================================================================
-    // ================These functions handle the offcanvas sidebar=============
-    // =========================================================================
-    var mobileView = 992;
-
-    $scope.getWidth = function() {
-        return window.innerWidth;
-    };
-
-    $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        if (newValue >= mobileView) {
-            if (angular.isDefined($cookieStore.get('toggle'))) {
-                $scope.toggle = ! $cookieStore.get('toggle') ? false : true;
-            } else {
-                $scope.toggle = true;
-            }
-        } else {
-            $scope.toggle = false;
-        }
-
-    });
-
-    $scope.toggleSidebar = function() {
-        $scope.toggle = !$scope.toggle;
-        $cookieStore.put('toggle', $scope.toggle);
-    };
-
-    window.onresize = function() {
-        $scope.$apply();
-    };
   };
 
   // ===========================================================================
--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,21 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 $(document).on('click',function(){
 	$('.collapse').collapse('hide');
 });
+
+
+$(document).on('click', '.toggle-sidebar', function() {
+  toggleSideBar();
+});
+
+
+function toggleSideBar() {
+
+    if ($('#page-wrapper').hasClass('show-sidebar')) {
+        // Do things on Nav Close
+        $('#page-wrapper').removeClass('show-sidebar');
+    } else {
+        // Do things on Nav Open
+        $('#page-wrapper').addClass('show-sidebar');
+    }
+    //$('#site-wrapper').toggleClass('show-nav');
+}
